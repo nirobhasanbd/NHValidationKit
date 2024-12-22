210d39b24f8f9e5fb8aabc811a3ea682a2e87d46
Nirob Hasan <hasan53nirob@gmail.com>
2024-12-22 12:44:20 +0600

    Added empty framework

---
+++ NHValidationKit/NHValidator.swift
@@ -0,0 +1,14 @@
+//
+//  NHValidator.swift
+//  NHValidationKit
+//
+//  Created by Md Nirob Hasan on 21/12/24.
+//
+
+import Foundation
+
+public class NHValidator {
+    public static func printMessage(_ message: String) {
+        print(message)
+    }
+}
---
+++ NHValidationTestApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  NHValidationTestApp
+//
+//  Created by Md Nirob Hasan on 21/12/24.
+//
+
+import UIKit
+import NHValidationKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        NHValidator.printMessage("Hello World")
+    }
+
+
+}
+
